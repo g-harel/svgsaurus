aed4859652e7ca1b978498f0f0a63406de625258
Gabriel <gabrielj.harel@gmail.com>
2018-07-14 17:16:10 -0400

    remove embedded fonts

--- main.go
+++ main.go
@@ -1,28 +1,17 @@
 package main
 
 import (
-	"encoding/base64"
 	"html/template"
-	"io/ioutil"
 	"os"
-	"path"
 	"strings"
 )
 
-// Template string used to render the output.
+// Template string.
 var t = `
 <svg xmlns="http://www.w3.org/2000/svg"
 	 height="{{.Height}}"
 	 viewBox="0 0 {{.Width}} {{.Height}}">
 	<style>
-		{{if .FontData -}}
-			<![CDATA[
-				@font-face {
-					font-family: '{{.Font}}';
-					src: url('data:application/x-font-ttf;base64,{{.FontData}}');
-				}
-			]]>
-		{{- end}}
 		text {
 			font-family: {{.Font}}, sans-serif;
 			font-size: {{.Size}}px;
@@ -60,8 +49,6 @@ type Config struct {
 	Bold       bool
 	Italic     bool
 	Underline  bool
-
-	FontData string
 }
 
 // FromMap populates the Config's values from a query string type.
@@ -94,47 +81,6 @@ func (c *Config) FromMap(m map[string][]string) *Config {
 	replace := fallback("r", "_")
 	c.Text = strings.Replace(c.Text, replace, " ", -1)
 	c.Font = strings.Replace(c.Font, replace, " ", -1)
-
-	return c
-}
-
-// LoadFontData attempts to load font data into the config.
-func (c *Config) LoadFontData() *Config {
-	parts := []string{c.Font}
-	if c.Bold {
-		parts = append(parts, "bold")
-	}
-	if c.Italic {
-		parts = append(parts, "italic")
-	}
-	name := strings.Join(parts, "-")
-
-	dir, err := os.Getwd()
-	if err != nil {
-		return c
-	}
-
-	preserveTransforms := false
-	raw, err := ioutil.ReadFile(path.Join(dir, "fonts", name+".ttf"))
-	if err != nil {
-		// Do not retry if there were no transforms
-		if c.Font == name {
-			return c
-		}
-		raw, err = ioutil.ReadFile(path.Join(dir, "fonts", c.Font+".ttf"))
-		if err != nil {
-			return c
-		}
-		preserveTransforms = true
-	}
-
-	c.FontData = base64.StdEncoding.EncodeToString(raw)
-
-	// Font is already transformed in ttf file.
-	if !preserveTransforms {
-		c.Bold = false
-		c.Italic = false
-	}
 
 	return c
 }
