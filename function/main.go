8c299d0f6d58663696235d16cd7d64a0cd7ba75d
Gabriel <gabrielj.harel@gmail.com>
2018-07-15 00:55:11 -0400

    add gcp cloud function handler

---
+++ function/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/cloud-functions-go/nodego"
+	"github.com/g-harel/svgsaurus"
+)
+
+func main() {
+	flag.Parse()
+	defer nodego.TakeOver()
+
+	http.HandleFunc(nodego.HTTPTrigger, func(w http.ResponseWriter, r *http.Request) {
+		svg, err := new(svgsaurus.Config).FromQuery(r.URL.Query()).Render()
+		if err != nil {
+			nodego.ErrorLogger.Print(err)
+			status := http.StatusInternalServerError
+			http.Error(w, http.StatusText(status), status)
+			return
+		}
+
+		w.Header().Add("Content-Type", "image/svg+xml")
+		w.Write(svg)
+	})
+}
